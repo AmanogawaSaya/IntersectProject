d464388bbe057421931ae5a241f75eb23783deaf
Tsuki <601470429@qq.com>
2020-03-04 20:49:29 +0800

    finished v0.1 with line solution

---
+++ include/main.h
@@ -0,0 +1,49 @@
+#pragma once
+
+struct dot {
+	double x;
+	double y;
+
+	bool operator<(const dot& rhs) const {
+		return x + y < rhs.x + rhs.y;
+	}
+};
+
+/* line is y = kx + b -> Ax + By + C = 0*/
+class line {
+private:
+	double k;
+	double b;
+
+public:
+	line(double x1, double y1, double x2, double y2) {
+		line::k = (y2 - y1) / (x2 - x1);
+		line::b = y1 - line::k * x1;
+	}
+
+	double getK() {
+		return k;
+	}
+
+	double getB() {
+		return b;
+	}
+};
+
+class circle {
+private:
+	int x;
+	int y;
+	int r;
+
+public:
+	circle(int x, int y, int r) {
+		circle::x = x;
+		circle::y = y;
+		circle::r = r;
+	}
+
+};
+
+void input(int num);
+struct dot calculate(double k1, double b1, double k2, double b2);
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include"../include/main.h"
+using namespace std;
+
+vector<line>lineArray;
+vector<circle>circleArray;
+set<struct dot> crossDot;
+
+void input(int num) {
+	for (int i = 0; i < num; i++) {
+		char type;
+		cin >> type;
+		if (type == 'L') {
+			int x1, y1, x2, y2;
+			cin >> x1 >> y1 >> x2 >> y2;
+			lineArray.push_back(line(x1, y1, x2, y2));
+		}
+		else if (type == 'C') {
+			int x, y, r;
+			cin >> x >> y >> r;
+			circleArray.push_back(circle(x, y, r));
+		}
+	}
+}
+
+struct dot calculate(double k1, double b1, double k2, double b2) {
+	struct dot result;
+	result.x = (b2 - b1) / (k2 - k1);
+	result.y = k1 * result.x + b1;
+	return result;
+}
+
+int main() {
+	int num = 0;
+	cin >> num;
+	input(num);
+	for (unsigned int i = 0; i < lineArray.size(); i++) {
+		for (unsigned int j = i + 1; j < lineArray.size(); j++) {
+			crossDot.insert(calculate(lineArray[i].getK(), lineArray[i].getB(), lineArray[j].getK(), lineArray[j].getB()));
+		}
+	}
+	cout << crossDot.size() << endl;
+	return 0;
+}
